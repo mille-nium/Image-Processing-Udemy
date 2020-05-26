d0ed3eeb29ba61fe8a2028a07be368662e842a0b
mille-nium <dianaboloten.db@gmail.com>
2020-05-26 19:58:12 +0300

    feat: add gray level transformation

--- gray_level_transformation.py
+++ gray_level_transformation.py
@@ -1,5 +1,23 @@
 from PIL import Image
+from pylab import *
 
-img = Image.open('images/bird.jpg')
+im = array(Image.open('images/bird.jpg').convert('L'))
 
-img.show()
+gray()
+
+negative_im = 255 - im
+contrast_im = 255.0 * (im / 255.0)**2
+
+subplot(1, 3, 1)
+imshow(im)
+title('Original')
+
+subplot(1, 3, 2)
+imshow(negative_im)
+title('Negative')
+
+subplot(1, 3, 3)
+imshow(contrast_im)
+title('High contrast')
+
+show()
