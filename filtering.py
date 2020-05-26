fea57c4dc8df22b0fd2e17c4684d6da6317de222
mille-nium <dianaboloten.db@gmail.com>
2020-05-26 19:57:37 +0300

    feat: add image filtering with different modes

--- filtering.py
+++ filtering.py
@@ -2,76 +2,36 @@ from PIL import Image
 from PIL import ImageFilter
 from pylab import *
 
-im0  = Image.open('images/moon.jpg')
+im0 = Image.open('images/city.jpg')
 figure(figsize =(15,15))
 
-subplot(3,4,1)
-plt.imshow(im0)
-plt.title('Original')
+subplot(2, 3, 1)
+imshow(im0)
+title('Original')
 
-subplot(3,4,2)
-im2 =  im0.filter(ImageFilter.CONTOUR)
-plt.imshow(im2)
-plt.title('Contour')
+subplot(2, 3, 2)
+im1 =  im0.filter(ImageFilter.CONTOUR)
+imshow(im1)
+title('Contour')
 
-subplot(3,4,3)
-im3 =  im0.filter(ImageFilter.DETAIL)
-plt.imshow(im3)
-plt.title('Detail')
+subplot(2, 3, 3)
+im2 =  im0.filter(ImageFilter.DETAIL)
+imshow(im2)
+title('Detail')
 
-subplot(3,4,4)
-im4  = im0.filter(ImageFilter.EDGE_ENHANCE)
-plt.imshow(im4)
-plt.title('Edge Enhance')
+subplot(2, 3, 4)
+im3 = im0.filter(ImageFilter.EMBOSS)
+imshow(im3)
+title('Emboss')
 
-subplot(3,4,5)
-im5 = im0.filter(ImageFilter.EDGE_ENHANCE_MORE)
-plt.imshow(im5)
-plt.title('Edge Enhance More')
+subplot(2, 3, 5)
+im4 =  im0.filter(ImageFilter.SMOOTH_MORE)
+imshow(im4)
+title('Smooth')
 
-subplot(3,4,6)
-im6 = im0.filter(ImageFilter.EMBOSS)
-plt.imshow(im6)
-plt.title('Emboss')
+subplot(2, 3, 6)
+im5 = im0.filter(ImageFilter.SHARPEN)
+imshow(im5)
+title('Sharpen')
 
-subplot(3,4,7)
-im7 =  im0.filter(ImageFilter.FIND_EDGES)
-plt.imshow(im7)
-plt.title('Find Edges')
-
-subplot(3,4,8)
-im8 =  im0.filter(ImageFilter.SMOOTH)
-plt.imshow(im8)
-plt.title('Lowpass 1')
-
-subplot(3,4,9)
-im9 =  im0.filter(ImageFilter.SMOOTH_MORE)
-plt.imshow(im9)
-plt.title('Lowpass 2')
-
-subplot(3,4,10)
-im10 = im0.filter(ImageFilter.SHARPEN)
-plt.imshow(im10)
-plt.title('Sharpen')
-
-
-#Custom kernels
-
-
-size = (3,3)
-kernel1  = [1,1,1,1,-1,1,-1,-1,-1]
-ker1 =  ImageFilter.Kernel(size,kernel1,scale=None,offset =0)
-subplot(3,4,11)
-im11  =  im0.filter(ker1)
-plt.imshow(im11)
-plt.title('Custom Filter 1')
-
-
-kernel2 = [1,0,-1,1,0,-1,0,0,-1]
-ker2 = ImageFilter.Kernel(size,kernel2,scale =None,offset=0)
-subplot(3,5,12)
-im12 =  im0.filter(ker2)
-plt.imshow(im12)
-plt.title('Custom filter 2')
-
-plt.show()
+show()
